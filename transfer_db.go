4cf6e0f33e0e41aaaa673185ef2b0227213268f1
Denis Sirotkin <sirotkin@denis-02.ru>
2024-08-26 16:41:41 +0300

    [/] add logic to copy ddl

--- transfer_db.go
+++ transfer_db.go
@@ -37,8 +37,8 @@ func checkConnection(config DBConfig) error {
 	return nil
 }
 
-// transferDatabase transfers the data from the source database to the target database
-func transferDatabase(sourceConfig, targetConfig DBConfig) error {
+// transferDatabaseSchema transfers the schema from the source database to the target database
+func transferDatabaseSchema(sourceConfig, targetConfig DBConfig) error {
 	log.Printf("Starting pg_dump from source database: %s on host: %s", sourceConfig.DBName, sourceConfig.Host)
 	// Create the pg_dump command to export the source database
 	dumpCmd := exec.Command("pg_dump",
@@ -48,6 +48,88 @@ func transferDatabase(sourceConfig, targetConfig DBConfig) error {
 		"-F", "c",
 		"-b",
 		"-v",
+		"--schema-only",
+		sourceConfig.DBName)
+
+	// Set the environment variable for the source database password
+	dumpCmd.Env = append(os.Environ(), fmt.Sprintf("PGPASSWORD=%s", sourceConfig.Password))
+
+	// Create the pg_restore command to import the data into the target database
+	restoreCmd := exec.Command("pg_restore",
+		"-h", targetConfig.Host,
+		"-p", targetConfig.Port,
+		"-U", targetConfig.User,
+		"-d", targetConfig.DBName,
+		"-v")
+
+	// Set the environment variable for the target database password
+	restoreCmd.Env = append(os.Environ(), fmt.Sprintf("PGPASSWORD=%s", targetConfig.Password))
+	var err error
+	// Link the output stream of pg_dump to the input stream of pg_restore
+	restoreCmd.Stdin, err = dumpCmd.StdoutPipe()
+	if err != nil {
+		return fmt.Errorf("error creating pipe: %v", err)
+	}
+
+	// Capture the stderr output of pg_dump
+	dumpStderr, err := dumpCmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("error capturing stderr from pg_dump: %v", err)
+	}
+
+	// Capture the stderr output of pg_restore
+	restoreStderr, err := restoreCmd.StderrPipe()
+	if err != nil {
+		return fmt.Errorf("error capturing stderr from pg_restore: %v", err)
+	}
+
+	// Log any errors from pg_dump
+	go func() {
+		scanner := bufio.NewScanner(dumpStderr)
+		for scanner.Scan() {
+			log.Printf("pg_dump: %s", scanner.Text())
+		}
+	}()
+
+	// Log any errors from pg_restore
+	go func() {
+		scanner := bufio.NewScanner(restoreStderr)
+		for scanner.Scan() {
+			log.Printf("pg_restore: %s", scanner.Text())
+		}
+	}()
+
+	// Start the pg_restore command before pg_dump to ensure it is ready to receive data
+	if err = restoreCmd.Start(); err != nil {
+		return fmt.Errorf("error starting pg_restore: %v", err)
+	}
+
+	// Start the pg_dump command
+	if err = dumpCmd.Start(); err != nil {
+		return fmt.Errorf("error starting pg_dump: %v", err)
+	}
+
+	// Wait for pg_dump to finish
+	if err = dumpCmd.Wait(); err != nil {
+		return fmt.Errorf("error waiting for pg_dump: %v", err)
+	}
+
+	log.Println("Database successfully transferred")
+	return nil
+}
+
+// transferDatabaseData transfers the data from the source database to the target database
+func transferDatabaseData(sourceConfig, targetConfig DBConfig) error {
+	log.Printf("Starting pg_dump from source database: %s on host: %s", sourceConfig.DBName, sourceConfig.Host)
+	// Create the pg_dump command to export the source database
+	dumpCmd := exec.Command("pg_dump",
+		"-h", sourceConfig.Host,
+		"-p", sourceConfig.Port,
+		"-U", sourceConfig.User,
+		"-F", "c",
+		"-b",
+		"-v",
+		"--data-only",
 		sourceConfig.DBName)
 
 	// Set the environment variable for the source database password
@@ -138,9 +220,15 @@ func main() {
 	}
 	log.Println("Successfully connected to target database")
 
+	// Transfer the schema from the source database to the target database
+	if err = transferDatabaseSchema(sourceConfig, targetConfig); err != nil {
+		log.Printf("Failed to transfer schema database: %v\n", err)
+		return
+	}
+
 	// Transfer the data from the source database to the target database
-	if err = transferDatabase(sourceConfig, targetConfig); err != nil {
-		log.Printf("Failed to transfer database: %v\n", err)
+	if err = transferDatabaseData(sourceConfig, targetConfig); err != nil {
+		log.Printf("Failed to transfer data database: %v\n", err)
 		return
 	}
 
